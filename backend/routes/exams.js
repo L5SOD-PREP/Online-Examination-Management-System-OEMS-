e1a451eed175b0117924302021faffceb11a95d1
ISHIMWE Andy <ishmukazi@gmail.com>
2026-05-20 10:57:07 +0200

    updating backend of exam structure

--- backend/routes/exams.js
+++ backend/routes/exams.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const authMiddleware = require('../middleware/auth');
+const { authMiddleware, requireTeacherOrAdmin } = require('../middleware/auth');
 const { getAllExams, getExamById, createExam, updateExam, deleteExam } = require('../controllers/examController');
 
-router.get('/', getAllExams);
-router.get('/:id', getExamById);
-router.post('/', createExam);
-router.put('/:id', updateExam);
-router.delete('/:id', deleteExam);
+router.get('/', authMiddleware, getAllExams);
+router.get('/:id', authMiddleware, getExamById);
+router.post('/', authMiddleware, requireTeacherOrAdmin, createExam);
+router.put('/:id', authMiddleware, requireTeacherOrAdmin, updateExam);
+router.delete('/:id', authMiddleware, requireTeacherOrAdmin, deleteExam);
 
 module.exports = router;
--- backend/routes/questions.js
+++ backend/routes/questions.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const authMiddleware = require('../middleware/auth');
+const { authMiddleware, requireTeacherOrAdmin } = require('../middleware/auth');
 const { getQuestionsByExam, getQuestionById, createQuestion, updateQuestion, deleteQuestion } = require('../controllers/questionController');
 
-router.get('/exam/:examId', getQuestionsByExam);
-router.get('/:id', getQuestionById);
-router.post('/', createQuestion);
-router.put('/:id', updateQuestion);
-router.delete('/:id', deleteQuestion);
+router.get('/exam/:examId', authMiddleware, getQuestionsByExam);
+router.get('/:id', authMiddleware, getQuestionById);
+router.post('/', authMiddleware, requireTeacherOrAdmin, createQuestion);
+router.put('/:id', authMiddleware, requireTeacherOrAdmin, updateQuestion);
+router.delete('/:id', authMiddleware, requireTeacherOrAdmin, deleteQuestion);
 
 module.exports = router;
